2a6237383b2681e264b25db4d6c20d0395676768
Fernando Elias <ferelias_1077@hotmail.com>
2023-06-10 18:39:36 -0600

    Arreglando un error gramatical

--- src/components/Portfoliofrontend.jsx
+++ src/components/Portfoliofrontend.jsx
@@ -62,7 +62,7 @@ const Portfoliofrontend = () => {
                     <p>Random Fox</p>
                 </div>
                 <div className="descripcion">
-                    <p className='des-proyect'>Este es un proyecto que utiliza la API RandomFox para mostrar imágenes aleatorias de zorros en una página web. Las cuáles se muestran y se borran de pantalla por medio de un botones.
+                    <p className='des-proyect'>Este es un proyecto que utiliza la API RandomFox para mostrar imágenes aleatorias de zorros en una página web. Las cuáles se muestran y se borran de pantalla por medio de unos botones.
 
 Para mejorar la velocidad de carga de la página, se ha implementado la técnica de lazy loading en las imágenes. De esta manera, se retrasa la carga de las imágenes hasta que el usuario las necesita o las solicita explícitamente, lo que mejora significativamente el tiempo de carga de la página..</p>
                 </div>
